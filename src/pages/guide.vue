c3f875128f14c38627491077b6a2f2bd9e772cd5
Lyserith <Lyserith@pm.me>
2022-09-04 21:03:33 +0200

    remove text-black

--- src/pages/guide.vue
+++ src/pages/guide.vue
@@ -48,28 +48,28 @@
 		<Description class="col-span-full">
 			<h2 class="mx-auto uppercase" style="font-weight: 300;">guide</h2>
 			<Card name="Wallet">
-				<p class="text-black">
+				<p>
 					How to setup a pea-core wallet.
 					<br>
 					<router-link to="/guide/wallet">Read more</router-link>
 				</p>
 			</Card>
 			<Card name="How to setup a node?">
-				<p class="text-black">
+				<p>
 					How to setup and host your very own node/validator.
 					<br>
 					<router-link to="/guide/node">Read more</router-link>
 				</p>
 			</Card>
 			<Card name="Report a bug">
-				<p class="text-black">
+				<p>
 					How to submit a bug report.
 					<br>
 					<router-link to="/guide/bug-report">Read more</router-link>
 				</p>
 			</Card>
 			<Card name="Developers">
-				<p class="text-black">
+				<p>
 					For developers of the project.
 					<br>
 					<router-link to="/guide/developers">Read more</router-link>
