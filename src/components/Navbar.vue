45fd7d8b07a0d2f27ce6fe3621e0abc50241d9db
Lyserith <Lyserith@pm.me>
2022-10-16 00:39:02 +0200

    updates

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -48,7 +48,7 @@
 						"
 						rel="noopener"
 					>
-						{{ link.text }}
+						<span class="mr-2">{{ link.text }}</span><heroicons-outline-external-link class="h-5 w-5" />
 					</a>
 				</nav>
 			</div>
@@ -62,16 +62,12 @@
 				open: false,
 				links: {
 					to: [
-						// {
-							// text: "Home",
-							// to: "/"
-						// }
+						{
+							text: "Home",
+							to: "/"
+						}
 					],
 					href: [
-						{
-							text: "Wallet",
-							href: "https://wallet.pea.cash"
-						},
 						{
 							text: "Explorer",
 							href: "https://explorer.pea.cash"
